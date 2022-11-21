e659445425067abfffe9af0d29100b33f21328ca
Vladimir Perfilev <persoff68@gmail.com>
2022-11-21 19:54:04 +0100

    TDM-159 enable image caching

--- src/main/java/com/persoff68/fatodo/web/rest/util/ResponseUtils.java
+++ src/main/java/com/persoff68/fatodo/web/rest/util/ResponseUtils.java
@@ -3,9 +3,12 @@ package com.persoff68.fatodo.web.rest.util;
 import org.bson.types.Binary;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
+import java.util.concurrent.TimeUnit;
 
 public class ResponseUtils {
     private static final String THUMBNAIL_PREFIX = "thumbnail-";
@@ -21,6 +24,7 @@ public class ResponseUtils {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + filename)
                 .contentLength(bytes.length)
                 .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .cacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
                 .body(resource);
     }
 
